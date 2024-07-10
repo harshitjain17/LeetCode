6244e62c165c1df1cb187a6df156cacd2b329c05
Harshit Jain <harshitj.cs@gmail.com>
2024-07-09 22:00:25 -0400

    :ballot_box_with_check: missing-number [Runtime: 92 ms; Memory: 12.7 MB]

--- array/missing-number.py
+++ array/missing-number.py
@@ -4,10 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # Method 1: Using sets
-        numset = set(nums)
-        trivialset = set()
+        # # Method 1: Using sets
+        # numset = set(nums)
+        # trivialset = set()
+        # for i in range(len(nums)+1):
+        #     trivialset.add(i)
+        # return trivialset.difference(numset).pop()
+
+        # Method 2: Sum
+        trivialsum, numssum = 0, 0
         for i in range(len(nums)+1):
-            trivialset.add(i)
-        return trivialset.difference(numset).pop()
-    
+            trivialsum += i
+        for i in range(len(nums)):
+            numssum += nums[i]
+        
+        value = (trivialsum - numssum) if trivialsum != numssum else 0
+        return value
