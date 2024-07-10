5c4a4cb3ffacd5bc8264f8d64960cf5ea8c4df1b
Harshit Jain <harshitj.cs@gmail.com>
2024-07-09 22:07:42 -0400

    :ballot_box_with_check: missing-number [Runtime: 92 ms; Memory: 12.8 MB]

--- binary-search/missing-number.py
+++ binary-search/missing-number.py
@@ -11,12 +11,19 @@ class Solution(object):
         #     trivialset.add(i)
         # return trivialset.difference(numset).pop()
 
-        # Method 2: Sum
-        trivialsum, numssum = 0, 0
+        # # Method 2: Sum (difference of sum of nums and trivial)
+        # trivialsum, numssum = 0, 0
+        # for i in range(len(nums)+1):
+        #     trivialsum += i
+        # for i in range(len(nums)):
+        #     numssum += nums[i]
+        # value = (trivialsum - numssum) if trivialsum != numssum else 0
+        # return value
+
+        # Method 3: XOR Operation (XOR all the numbers in nums including trivial list)
+        res = nums[0]
+        for i in range(1, len(nums)):
+            res = res ^ nums[i]
         for i in range(len(nums)+1):
-            trivialsum += i
-        for i in range(len(nums)):
-            numssum += nums[i]
-        
-        value = (trivialsum - numssum) if trivialsum != numssum else 0
-        return value
+            res = res ^ i
+        return res
